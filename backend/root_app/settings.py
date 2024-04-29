e6bafe024006e48f242ed2d21d8e9d4dc7412000
nahid na <absiddik718@gmail.com>
2024-04-30 00:03:28 +0600

    fixed configuration

--- backend/root_app/settings.py
+++ backend/root_app/settings.py
@@ -32,9 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-print("Debug : ", DEBUG)
-
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 
 
 # Application definition
