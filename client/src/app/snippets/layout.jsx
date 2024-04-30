2043bd8a9f52a1858c52430c07f682fcc31a9eba
nahid na <absiddik718@gmail.com>
2024-04-30 19:13:22 +0600

    added meta data

---
+++ client/src/app/snippets/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Your code snippets',
+    description: 'created by nextjs',
+}
+
+export default function DashboardLayout({ children }) {
+    return (
+        <section>
+            {children}
+        </section>
+    )
+}
